f7752ef954f5310548c843111bd32f020ebbac56
DelamareA <arnaud.delamare@epfl.ch>
2016-03-23 11:18:19 +0100

    Spot complete

--- icg16/hw4_shading/mesh/spot_fshader.glsl
+++ icg16/hw4_shading/mesh/spot_fshader.glsl
@@ -1,19 +1,18 @@
 #version 330
 
-
-out vec3 color;
+in vec3 normal_mv;
+in vec3 light_dir;
+in vec3 view_dir;
+in float spot_effect_not_pow;
 
 uniform vec3 La, Ld, Ls;
 uniform vec3 ka, kd, ks;
 uniform float alpha;
 
+out vec3 color;
+
 const float spot_cos_cutoff = 0.985; // cos 10 degrees
 const float spot_exp = 150;
-
-in vec3 normal_mv;
-in vec3 light_dir;
-in vec3 view_dir;
-in float spot_effect_sns_exp;
 
 void main() {    
     const vec3 COLORS[6] = vec3[](
@@ -29,18 +28,18 @@ void main() {
     //>>>>>>>>>> TODO >>>>>>>>>>>
     // TODO 5: Spot light.
     // Complete the shader to obtain spot light effect
-    vec3 petit_n = dot(light_dir, normal_mv) * normal_mv;
-    vec3 r = normalize((petit_n - light_dir) + petit_n);
+    //<<<<<<<<<< TODO <<<<<<<<<<<
+
+    vec3 proj = dot(light_dir, normal_mv) * normal_mv;
+    vec3 r_dir = normalize(2 * proj - light_dir);
 
     float nl = dot(normal_mv, light_dir);
-    nl = nl < 0 ? 0 : nl;
-    float rv = dot(r, view_dir);
-    rv = rv < 0 ? 0 : rv;
-    color = ka * La + kd * nl * Ld + ks * pow(rv, alpha) * Ls;
+    nl = nl >= 0 ? nl : 0;
 
-    float spot_effect = pow(spot_effect_sns_exp, spot_exp);
+    float rv = dot(r_dir, view_dir);
+    rv = rv >= 0 ? rv : 0;
 
-    color *= spot_effect;
+    float spot_effect = spot_effect_not_pow >= spot_cos_cutoff ? pow(spot_effect_not_pow, spot_exp) : 0;
 
-    //<<<<<<<<<< TODO <<<<<<<<<<<
+    color = ka * La + (kd * nl * Ld + ks * pow(rv, alpha) * Ls) * spot_effect;
 }
--- icg16/hw4_shading/mesh/spot_vshader.glsl
+++ icg16/hw4_shading/mesh/spot_vshader.glsl
@@ -13,20 +13,21 @@ uniform vec3 spot_dir;
 out vec3 normal_mv;
 out vec3 light_dir;
 out vec3 view_dir;
-out float spot_effect_sns_exp;
-
+out float spot_effect_not_pow;
 
 void main() {
     mat4 MV = view * model;
     vec4 vpoint_mv = MV * vec4(vpoint, 1.0);
     gl_Position = projection * vpoint_mv;
-
+    ///>>>>>>>>>> TODO >>>>>>>>>>>
+    /// TODO 1.1: Phong shading.
     /// 1) compute normal_mv using the model_view matrix.
-    normal_mv = normalize((inverse(transpose(MV)) * vec4(vnormal, 1.0)).xyz);
-    // 2) compute the light direction light_dir.
-    light_dir = normalize(light_pos - vpoint_mv.xyz);
-    // 3) compute the view direction view_dir.
-    view_dir = normalize(-vpoint_mv.xyz);
+    /// 2) compute the light direction light_dir.
+    /// 3) compute the view direction view_dir.
+    ///<<<<<<<<<< TODO <<<<<<<<<<<
 
-    spot_effect_sns_exp = dot(light_dir,spot_dir);
+    normal_mv = normalize((inverse(transpose(MV)) * vec4(vnormal, 1.0)).xyz);
+    light_dir = normalize(light_pos - vpoint_mv.xyz);
+    view_dir = normalize(-vpoint_mv.xyz);
+    spot_effect_not_pow = dot(normalize(spot_dir), light_dir);
 }
