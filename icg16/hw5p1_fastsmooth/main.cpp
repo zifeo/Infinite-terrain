9363dc5202d9f8287d753ee141c3f53616b27613
Teo Stocco <teo.stocco@epfl.ch>
2016-03-24 14:59:27 +0100

    add homework 5

---
+++ icg16/hw5p1_fastsmooth/main.cpp
@@ -0,0 +1,185 @@
+// glew must be before glfw
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// contains helper functions such as shader compiler
+#include "icg_helper.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "framebuffer.h"
+
+#include "cube/cube.h"
+#include "quad/quad.h"
+#include "screenquad/screenquad.h"
+
+Cube cube;
+Quad quad;
+
+int window_width = 1280;
+int window_height = 720;
+
+using namespace glm;
+
+mat4 projection_matrix;
+
+FrameBuffer framebuffer;
+FrameBuffer framebuffer2;
+ScreenQuad screenquad;
+
+void Init(GLFWwindow* window) {
+    glClearColor(1.0, 1.0, 1.0 /*white*/, 1.0 /*solid*/);
+    glEnable(GL_DEPTH_TEST);
+
+    cube.Init();
+    quad.Init();
+
+    float ratio = window_width / (float) window_height;
+    projection_matrix = perspective(45.0f, ratio, 0.1f, 10.0f);
+
+    // on retina/hidpi displays, pixels != screen coordinates
+    // this unsures that the framebuffer for the motion blur effect
+    // has the same size as the window
+    // (see http://www.glfw.org/docs/latest/window.html#window_fbsize)
+    glfwGetFramebufferSize(window, &window_width, &window_height);
+
+    GLuint fb_tex = framebuffer.Init(window_width, window_height);
+    GLuint fb_tex2 = framebuffer2.Init(window_width, window_height);
+
+    screenquad.Init(window_width, window_height, fb_tex, fb_tex2);
+}
+
+void Display() {
+    glViewport(0,0,window_width,window_height);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // setup view-projection matrix
+    vec3 cam_pos(2.0f, 2.0f, 2.0f);
+    vec3 cam_look(0.0f, 0.0f, 0.0f);
+    vec3 cam_up(0.0f, 0.0f, 1.0f);
+    mat4 view = lookAt(cam_pos, cam_look, cam_up);
+    mat4 VP = projection_matrix * view;
+    
+    // render to FB
+    framebuffer.Bind();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    cube.Draw(VP);
+    quad.Draw(VP);
+    framebuffer.Unbind();
+
+    framebuffer2.Bind();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    screenquad.Draw(PASS::FIRST); // first pass
+    framebuffer2.Unbind();
+
+    // render to Window
+    glViewport(0, 0, window_width, window_height);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    screenquad.Draw(PASS::SECOND); // second pass in shader
+}
+
+// gets called when the windows/framebuffer is resized.
+void ResizeCallback(GLFWwindow* window, int width, int height) {
+    window_width = width;
+    window_height = height;
+
+    float ratio = window_width / (float) window_height;
+    projection_matrix = perspective(45.0f, ratio, 0.1f, 10.0f);
+
+    glViewport(0, 0, window_width, window_height);
+    framebuffer.Cleanup();
+    framebuffer.Init(window_width, window_height);
+    framebuffer2.Cleanup();
+    framebuffer2.Init(window_width, window_height);
+    screenquad.UpdateSize(window_width, window_height);
+}
+
+void ErrorCallback(int error, const char* description) {
+    fputs(description, stderr);
+}
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (action != GLFW_RELEASE) {
+        return;
+    }
+    switch(key) {
+        case 'Q':
+            screenquad.UpdateStandardDeviation(0.75);
+            break;
+        case 'W':
+            screenquad.UpdateStandardDeviation(1.25);
+            break;
+        default:
+            break;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // GLFW Initialization
+    if(!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwSetErrorCallback(ErrorCallback);
+
+    // hint GLFW that we would like an OpenGL 3 context (at least)
+    // http://www.glfw.org/faq.html#how-do-i-create-an-opengl-30-context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // attempt to open the window: fails if required version unavailable
+    // note some Intel GPUs do not support OpenGL 3.2
+    // note update the driver of your graphic card
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height,
+                                          "Fast Smoothing", NULL, NULL);
+    if(!window) {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    // makes the OpenGL context of window current on the calling thread
+    glfwMakeContextCurrent(window);
+
+    // set the callback for escape key
+    glfwSetKeyCallback(window, KeyCallback);
+
+    // set the framebuffer resize callback
+    glfwSetFramebufferSizeCallback(window, ResizeCallback);
+
+    // GLEW Initialization (must have a context)
+    // https://www.opengl.org/wiki/OpenGL_Loading_Library
+    glewExperimental = GL_TRUE; // fixes glew error (see above link)
+    if(glewInit() != GLEW_NO_ERROR) {
+        fprintf( stderr, "Failed to initialize GLEW\n");
+        return EXIT_FAILURE;
+    }
+
+    cout << "OpenGL" << glGetString(GL_VERSION) << endl;
+
+    // initialize our OpenGL program
+    Init(window);
+
+    // render loop
+    while(!glfwWindowShouldClose(window)){
+        Display();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    cube.Cleanup();
+    quad.Cleanup();
+    framebuffer.Cleanup();
+    framebuffer2.Cleanup();
+    screenquad.Cleanup();
+
+    // close OpenGL window and terminate GLFW
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
