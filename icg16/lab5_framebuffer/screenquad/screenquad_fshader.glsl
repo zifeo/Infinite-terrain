ce8cd16c2854ec42aff503118674260f31bd5884
DelamareA <arnaud.delamare@epfl.ch>
2016-03-24 15:00:54 +0100

    Add gaussian blur

--- icg16/lab5_framebuffer/screenquad/screenquad_fshader.glsl
+++ icg16/lab5_framebuffer/screenquad/screenquad_fshader.glsl
@@ -13,8 +13,7 @@ float rgb_2_luma(vec3 c) {
 }
 
 void main() {
-
-    mat3 gx = mat3(-1.0f, -2.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 2.0f, 1.0f);
+    /*mat3 gx = mat3(-1.0f, -2.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 2.0f, 1.0f);
     mat3 gy = transpose(gx);
 
     float ix = 0;
@@ -40,6 +39,21 @@ void main() {
     iy += gy[2][2] * rgb_2_luma(textureOffset(tex, uv, ivec2(1, -1)).rgb);
 
     float iout = 1.0 - sqrt(ix * ix + iy * iy);
-    color = vec3(iout);
+    color = vec3(iout);*/
+
+    const float sigma = 2;
+    const int size = 11;
+
+    float tot_weight = 0;
+    vec3 tot_image = vec3(0);
+    for(int i = -size/2; i <= size/2; i++){
+        for(int j = -size/2; j <= size/2; j++){
+            float dist = sqrt(pow(5, 2) + pow(j, 2));
+            float weight = exp(-dist/(2 * sigma * sigma)); // gaussian
+            tot_image += weight * texture(tex, uv + vec2(i/tex_width,j/tex_height)).rgb;
+            tot_weight += weight;
+        }
+    }
+    color = tot_image / tot_weight;
 }
 
