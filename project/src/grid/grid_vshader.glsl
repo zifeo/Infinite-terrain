2ca50e72257b3f6ecbe767b495395170cfdddcd1
DelamareA <arnaud.delamare@epfl.ch>
2016-04-27 16:50:32 +0200

    Added basic textures

--- project/src/grid/grid_vshader.glsl
+++ project/src/grid/grid_vshader.glsl
@@ -3,11 +3,11 @@
 #define SKIRT 0.001
 
 in vec2 position;
+in vec2 vtexcoord;
 
 out vec2 uv;
-out vec3 colorHeight;
-
 out vec3 light_dir;
+out float height;
 
 uniform mat4 MVP;
 uniform mat4 MV;
@@ -15,19 +15,15 @@ uniform vec3 light_pos;
 uniform float time;
 
 uniform sampler2D tex;
-uniform sampler1D tex1D;
 
 void main() {
     uv = (position + vec2(1.0)) * 0.5;
-    float height = texture(tex, uv).x;
+    height = texture(tex, uv).x;
 
-    colorHeight = texture(tex1D, height).rgb;
+    vec3 pos_3d = vec3(position.x, height*2 - 1, -position.y);
 
-    vec3 pos_3d = vec3(position.x, height * 2 - 1, -position.y);
-
-    if (position.x < -1.0f + SKIRT || position.x > 1.0f - SKIRT || position.y < -1.0f + SKIRT ||
-        position.y > 1.0f - SKIRT)
-        pos_3d = vec3(pos_3d.x, pos_3d.y - 0.2, pos_3d.z);
+    if (position.x < -1.0f + SKIRT || position.x > 1.0f - SKIRT || position.y < -1.0f + SKIRT || position.y > 1.0f - SKIRT)
+        pos_3d = vec3(pos_3d.x, pos_3d.y-0.2, pos_3d.z);
 
     vec4 vpoint_mv = MV * vec4(pos_3d, 1.0);
     gl_Position = MVP * vec4(pos_3d, 1.0);
